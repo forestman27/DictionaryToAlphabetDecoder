0d5a6f8d509f85c1454f9ce8a133c76778a9bd9a
GuntherSchaaf <gunther.i.schaaf@gmail.com>
2021-09-20 16:02:16 -0400

    figured out the solution and now just working through the searching the graph for the longest path.

--- Testing/decoder.test.js
+++ Testing/decoder.test.js
@@ -1,14 +1,35 @@
 const Decoder = require('../src/Decoder.js');
 
 
+// It is impossible to figure out some cases.
+/**
+ * 
+ */
+test('example test given to make the graph', ()=> {
+    const words = ['bca','aaa','acb'];
+    let graph = new Map();
+    graph.set('b',['a']);
+    graph.set('a',['c']);
+    expect(Decoder.graph(words)).toStrictEqual(graph);
+})
+
 test('example test given', ()=> {
     const words = ['bca','aaa','acb'];
     const result = ['b','a','c'];
-    expect(Decoder.decode(words)).toStrictEqual(result);
+
+
+    expect(new Decoder(words).decode()).toStrictEqual(result);
 })
 
 test('US alphabet', () => {
-    const words = ['aba','acc','acd','ca','cd'];
+    const words = ['aba','accd','acd'];
     const results = ['a','b','c','d'];
-    expect(Decoder.decode(words)).toStrictEqual(results);
+    expect(new Decoder(words).decode()).toStrictEqual(results);
 })
+
+test('US alphabet 2', () => {
+    const words = ['aaa','accad','accd','fab','fac','fc'];
+    const results = ['a','b','c','d','f'];
+    expect(new Decoder(words).decode()).toStrictEqual(results);
+})
+
--- src/Decoder.js
+++ src/Decoder.js
@@ -1,10 +1,114 @@
-
-
+'use strict';
 class Decoder {
 
-    static decode(words) {
-        
+    constructor(words) {
+        this.words = words;
+    }
+    /**
+     * iterate all the words 
+     * for each new word add it to the result array
+     * @param {*} words 
+     */
+    /**
+     * Proof: if the index of the last word is the same as the current
+     *  then the last words next letter is before the currents next
+     * 
+     * if the indexs don't match between the words then it is impossible to tell which letter is next in the alphabet
+     * 
+     * Solution
+     * using the above proof create a directional graph and find the largest connection of indices
+     */
+    decode() {
+        return search(graph(this.words));
+    }
+
+    /**
+     * 
+     * @param {Adjacency List As JS Map} graph 
+     */
+    search(graph) {
+
+    };
+
+    static graph(words) {
+        let graph = new Map();
+        for (let i = 1; i < words.length; i++) {
+            let letterIndex = 0;
+            while ((words[i-1].length > letterIndex) && (words[i].charAt(letterIndex) === words[i-1].charAt(letterIndex))) {
+                letterIndex++;
+            }
+    
+            // we are going to point down the line a => b => c,d, c => d,... etc (then reverse it at the end of decode algorithm.)
+            if (words[i-1].length > letterIndex) {
+                let a = words[i].charAt(letterIndex);
+                let b = words[i - 1].charAt(letterIndex);
+                uppdateVertex(b, a);
+            } 
+        }
+        return graph;
+    
+    
+        function uppdateVertex(key, newValue) {
+            let vertexList = graph.get(key);
+            if (vertexList === undefined) {
+                graph.set(key, [newValue]);
+                
+            } else {
+                for (let i = 0; i < vertexList.length; i++) {
+                    if (vertexList[i] === newValue) {
+                        return;
+                    }
+                }
+                vertexList.push(newValue);
+                graph.set(key, vertexList);
+            }
+        }
     }
 }
 
 module.exports = Decoder;
+
+
+
+/**
+ * 
+ * @param {list of "ordered" words} words 
+ */
+function graph(words) {
+    let graph = new Map();
+    for (let i = 1; i < words.length; i++) {
+        let letterIndex = 0;
+        while ((words[i-1].length > letterIndex) && (words[i].charAt(letterIndex) === words[i-1].charAt(letterIndex))) {
+            letterIndex++;
+        }
+
+        // we are going to point down the line a => b => c,d, c => d,... etc (then reverse it at the end of decode algorithm.)
+        if (words[i-1].length > letterIndex) {
+            let a = words[i].charAt(letterIndex);
+            let b = words[i - 1].charAt(letterIndex);
+            uppdateVertex(b, a);
+        } 
+    }
+    return graph;
+
+
+    function uppdateVertex(key, newValue) {
+        let vertexList = graph.get(key);
+        if (vertexList === undefined) {
+            graph.set(key, [newValue]);
+            
+        } else {
+            for (let i = 0; i < vertexList.length; i++) {
+                if (vertexList[i] === newValue) {
+                    return;
+                }
+            }
+            vertexList.push(newValue);
+            graph.set(key, vertexList);
+        }
+    }
+}
+
+console.log(graph(['aba','accd','acd']))
+console.log(graph(['bca','aaa','acb']))
+console.log(graph(['aaa','accad','accd','fab','fac','fc']))
